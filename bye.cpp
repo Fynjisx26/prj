4b3109c9e6752607c656d25f5f5d7ff08eadaee1
Fynjisx26 <Fynjisx26@gmail.com>
2019-07-23 08:26:39 -0500

    First commit

---
+++ bye.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "bye.h"
+
+using namespace std;
+
+void print_bye(){
+   cout << "Good Bye!";
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "hello.h"
+#include "bye.h"
+
+using namespace std;
+
+int main(){
+    print_hello();
+    print_bye();
+    cout << endl;
+    //cout << "The factorial of 5 is " << factorial(5) << endl;
+    return 0;
+}
